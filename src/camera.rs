f038c027dcded13ee24c34f882263441fd4ff41b
Alienduck <oneuillyr@gmail.com>
2026-05-09 20:12:47 +0200

    new camera module to handle camera and window

---
+++ src/camera.rs
@@ -0,0 +1,54 @@
+use crate::player::Player;
+use bevy::{
+    input::mouse::MouseMotion,
+    prelude::*,
+    window::{CursorGrabMode, CursorOptions, PrimaryWindow, WindowFocused},
+};
+use std::f32::consts::FRAC_PI_2;
+
+pub struct CameraPlugin;
+
+impl Plugin for CameraPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(Update, (move_camera, toggle_cursor));
+    }
+}
+
+#[derive(Component)]
+pub struct PlayerCamera;
+
+fn move_camera(
+    mut mouse_motion: MessageReader<MouseMotion>,
+    mut player_query: Query<(&mut Transform, &mut Player)>,
+    mut camera_query: Query<&mut Transform, (With<PlayerCamera>, Without<Player>)>,
+    time: Res<Time>,
+) {
+    let Ok((mut transform, mut player)) = player_query.single_mut() else {
+        return;
+    };
+    let Ok(mut cam_transform) = camera_query.single_mut() else {
+        return;
+    };
+
+    for msg in mouse_motion.read() {
+        transform.rotate_y(-msg.delta.x * time.delta_secs() * player.motion_speed);
+        player.pitch = (player.pitch - msg.delta.y * time.delta_secs() * player.motion_speed)
+            .clamp(-FRAC_PI_2 + 0.01, FRAC_PI_2 - 0.01);
+        cam_transform.rotation = Quat::from_rotation_x(player.pitch);
+    }
+}
+
+fn toggle_cursor(
+    mut cursor_options: Single<&mut CursorOptions>,
+    mut window_event: MessageReader<WindowFocused>,
+) {
+    for window in window_event.read() {
+        if window.focused {
+            cursor_options.visible = false;
+            cursor_options.grab_mode = CursorGrabMode::Locked;
+        } else {
+            cursor_options.visible = true;
+            cursor_options.grab_mode = CursorGrabMode::None;
+        }
+    }
+}
