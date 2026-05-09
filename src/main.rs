66b51f21dbdf4faa565986c8d4a07df75886154d
Alienduck <oneuillyr@gmail.com>
2026-05-09 20:12:47 +0200

    simplify main

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use bevy::prelude::*;
 use bevy_rapier3d::prelude::*;
-use kinetic_game::player::PlayerPlugin;
+use kinetic_game::{camera::CameraPlugin, map::MapPlugin, player::PlayerPlugin};
 
 fn main() {
     App::new()
@@ -13,25 +13,6 @@ fn main() {
             ..default()
         }))
         .add_plugins(RapierPhysicsPlugin::<NoUserData>::default())
-        .add_plugins(PlayerPlugin)
-        .add_systems(Startup, setup_scene)
+        .add_plugins((MapPlugin, PlayerPlugin, CameraPlugin))
         .run();
-}
-
-fn setup_scene(mut commands: Commands, asset_server: Res<AssetServer>) {
-    commands.spawn((
-        DirectionalLight::default(),
-        Transform::from_xyz(0.0, 50.0, 0.0).looking_at(Vec3::ZERO, Vec3::Y),
-    ));
-
-    let map_handle: Handle<Scene> = asset_server.load("3Dmodels/fps_tps_map.glb#Scene0");
-
-    commands.spawn((
-        SceneRoot(map_handle),
-        Transform::default(),
-        AsyncSceneCollider {
-            shape: Some(ComputedColliderShape::TriMesh(TriMeshFlags::default())),
-            ..default()
-        },
-    ));
 }
